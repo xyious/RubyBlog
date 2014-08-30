a63863f0a5e36dba595b754be5b1f319265f5aee
Alexander Knopf <xyious@gmail.com>
2014-08-30 05:47:00 +0200

    first tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_match("Sample App", response.body, "home contains 'Sample App'")
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_match("Help", response.body, "help contains 'Help'")
   end
 
 end
